fc01207fcea6d48f4319c35a982de49988defd84
planetchili <planetchili@users.noreply.github.com>
2017-09-25 13:08:37 +0900

    better floor tiles (more variation)

--- Engine/Background.h
+++ Engine/Background.h
@@ -3,37 +3,68 @@
 #include "Graphics.h"
 #include "Surface.h"
 #include "SpriteEffect.h"
+#include <vector>
+#include <random>
 
 class Background
 {
 public:
-	Background( const RectI& rect )
+	Background( const RectI& bgRegion )
 		:
-		floor( "Images\\floor.bmp" ),
-		origin( rect.TopLeft() )
+		tileset( "Images\\floor5.bmp" ),
+		origin( bgRegion.TopLeft() )
 	{
 		// calculate grid dims so that there are enough tiles to fill screen
-		gridWidth = div_int_ceil( rect.GetWidth(),floor.GetWidth() );
-		gridHeight = div_int_ceil( rect.GetHeight(),floor.GetWidth() );
+		gridWidth = div_int_ceil( bgRegion.GetWidth(),tileSize );
+		gridHeight = div_int_ceil( bgRegion.GetHeight(),tileSize );
+		// generate tile rects
+		for( int n = 0; n < nTiles; n++ )
+		{
+			tileRects.emplace_back(
+				Vei2{ tileSize,0 } * n,tileSize,tileSize
+			);
+		}
+		// reserve tile map space
+		tiles.reserve( gridWidth * gridHeight );
+		// populate map with random floor tiles
+		std::mt19937 rng( std::random_device{}() );
+		std::uniform_int_distribution<int> dist( 0,nTiles - 1 );
+		for( int n = 0; n < gridWidth * gridHeight; n++ )
+		{
+			tiles.push_back( dist( rng ) );
+		}
 	}
 	void Draw( Graphics& gfx ) const
 	{
-		const auto tileWidth = floor.GetWidth();
-		const auto tileHeight = floor.GetHeight();
 		for( int y = 0; y < gridHeight; y++ )
 		{
 			for( int x = 0; x < gridWidth; x++ )
 			{
-				gfx.DrawSprite( x * tileWidth + origin.x,y * tileHeight + origin.y,floor,
-								SpriteEffect::Copy{} );
+				gfx.DrawSprite( x * tileSize + origin.x,y * tileSize + origin.y,
+								tileRects[GetTileAt( x,y )],
+								tileset,SpriteEffect::Copy{}
+				);
 			}
 		}
 	}
 private:
-	// floor tile art
-	Surface floor;
+	int GetTileAt( int x,int y ) const
+	{
+		return tiles[y * gridWidth + x];
+	}
+private:
+	// tileset image for background
+	Surface tileset;
 	// top left corner of background grid
 	Vei2 origin;
+	// vector of tile rects
+	std::vector<RectI> tileRects;
+	// grid of tiles (indices into the tileRect vector)
+	std::vector<int> tiles;
+	// number of tiles in set
+	int nTiles = 6;
+	// tile dimensions in pixels
+	int tileSize = 32;
 	// grid dimensions in tiles
 	int gridWidth;
 	int gridHeight;
