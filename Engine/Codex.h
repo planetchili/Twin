8906c572b62226061193e294c10d688cb40b87a0
planetchili <planetchili@users.noreply.github.com>
2017-10-18 15:58:18 +0900

    improve retrieval of resource when not in codex (no need to bin search 2x)

--- Engine/Codex.h
+++ Engine/Codex.h
@@ -23,11 +23,6 @@ private:
 		// this pointer owns the resource on the heap
 		// put the resources on the heap to keep them STABLE
 		const T* pResource;
-		// operator needed for binary search and lower bound
-		bool operator<( const Entry& rhs ) const
-		{
-			return key < rhs.key;
-		}
 	};
 public:
 	// retrieve a ptr to resource based on string (load if not exist)
@@ -52,37 +47,29 @@ private:
 	// retrieve a ptr to resource based on string (load if not exist)
 	const T* _Retrieve( const std::wstring& key )
 	{
-		// see if resource already exists in codex with binary search
-		const auto i = binary_find( entries.begin(),entries.end(),key,
-			[]( const Entry& e )
+		// find position of resource OR where resource should be (with bin search)
+		auto i = std::lower_bound( entries.begin(),entries.end(),key,
+			[]( const Entry& e,const std::wstring& key )
 			{
-				return e.key;
+				return e.key < key;
 			}
 		);
-		// if resource does not exist, load, store in sorted pos in codex, and return ptr
-		if( i == entries.end() )
+		// if resource does not exist (i.e. i does not point to resource with right key)
+		// load, store in sorted pos in codex, and set i to point to it
+		if( i == entries.end() || i->key != key )
 		{
-			// create an entry based on key and new (heap) loaded resource
-			const Entry e( key,new T( key ) );
-			// find sorted position in entries vector
-			const auto i = std::lower_bound( entries.begin(),entries.end(),e );
-			// insert entry
-			entries.insert( i,e );
-			// return pointer to resource
-			return e.pResource;
+			// construct entry in-place (i is the pos where the key *should* be)
+			i = entries.emplace( i,key,new T( key ) );
 		}
-		// else return ptr to existing resource in codex
-		else
-		{
-			return i->pResource;
-		}
+		// return ptr to resource in codex
+		return i->pResource;
 	}
 	// remove all entries from codex
 	void _Purge()
 	{
 		for( auto& e : entries )
 		{
-			delete e.presource;
+			delete e.pResource;
 		}
 		entries.clear();
 	}
